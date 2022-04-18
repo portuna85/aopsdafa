da6a030bfca0eb10df5eaa51d173780799ea7b7c
portuna85 <portuna85@gmail.com>
2022-04-19 07:19:04 +0900

    AOP Update

--- src/main/java/hello/aop/order/OrderRepository.java
+++ src/main/java/hello/aop/order/OrderRepository.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 public class OrderRepository {
     public String save(String itemId) {
         log.info("[orderRepository] 실행");
-//저장 로직
+        //저장 로직
         if (itemId.equals("ex")) {
             throw new IllegalStateException("예외 발생!");
         }
